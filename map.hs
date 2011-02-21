6a38bd73ce5b34bb34ff8faee1cb682994fbe58c
klaeufer@gmail.com <klaeufer@gmail.com@6eb1746a-ae27-11de-a8b3-6bed2ae3e0d9>
2011-02-21 03:14:36 +0000

    Added spreadsheet with performance analysis.

--- map.hs
+++ map.hs
@@ -30,21 +30,21 @@ randomChars gen = randomRs charRange gen
 randomKeySizes (k1, k2) gen = randomRs (k1, k2) gen
 
 randomKeysS :: (Int, Int) -> StdGen -> [String]
-randomKeysS (k1, k2) gen = 
+randomKeysS (k1, k2) gen =
   let
     (gen1, gen2) = split gen
   in
     spliterate (randomKeySizes (k1, k2) gen1) $ randomChars gen2
 
 -- successively bite off chunks from cGen of sizes supplied by sGen
--- cannot be a fold because it consumes two generators 
+-- cannot be a fold because it consumes two generators
 -- -> use scanl, which works on infinite lists
 spliterate :: [Int] -> [a] -> [[a]]
-spliterate sGen cGen = 
+spliterate sGen cGen =
   map fst $ drop 1 $ scanl (\(_, r) s -> splitAt s r) ([], cGen) sGen
 {-
-spliterate lGen cGen = 
-  let 
+spliterate lGen cGen =
+  let
     len : lRest = lGen
     (key, cRest) = splitAt len cGen
   in
@@ -53,16 +53,16 @@ spliterate lGen cGen =
 
 -- grow map m until it reaches size n from items supplied by src
 -- cannot be a fold because termination is based on result size
--- -> use scanl for producing successive values and find to pick 
+-- -> use scanl for producing successive values and find to pick
 -- the first one that satisfies our predicate
 growMap :: Ord a => Int -> [a] -> Map a a -> Map a a
-growMap n src m = 
-  fromJust $ find ((>= n) . size) $ 
+growMap n src m =
+  fromJust $ find ((>= n) . size) $
     scanl (\m' key -> Data.Map.insert key key m') m src
 {-
 -- recursive version for comparison
 growMap n src m | size m >= n = m
-                | otherwise = 
+                | otherwise =
                   let
                     key : src' = src
                     m' = Data.Map.insert key key m
@@ -75,8 +75,8 @@ mkMap n src = growMap n src empty
 
 -- shrink map m by given percentage
 shrinkMap :: Ord a => Int -> StdGen -> Map a a -> Map a a
-shrinkMap perc gen m = 
-  let 
+shrinkMap perc gen m =
+  let
     n = size m
     n1 = perc * n `div` 100
     ks = take n1 $ shuffle' (keys m) n gen
@@ -91,7 +91,7 @@ deleteKeys ks m = foldl' (flip Data.Map.delete) m ks
 -- recursive version for comparison
 deleteKeys [] m =
 deleteKeys (key : src) m =
-  let 
+  let
     m' = Data.Map.delete key m
   in
     deleteKeys src m'
@@ -112,30 +112,27 @@ main = do
   print $ show $ take 100 $ randomKeySizes (k1, k2) gen
   print $ show $ take 100 $ randomKeysS (k1, k2) gen
 -}
-  
+
   print "creating"
   time1 <- getCurrentTime
-  let map1 = mkMap n $ randomKeysS (k1, k2) $ mkStdGen 1
+  let !map1 = mkMap n $ randomKeysS (k1, k2) $ mkStdGen 1
   print $ show $ size map1
-  print $ show $ maximum $ elems map1
   time2 <- getCurrentTime
   print $ show $ diffUTCTime time2 time1
-  
+
   print "shrinking"
   time1 <- getCurrentTime
-  let map2 = shrinkMap d (mkStdGen 2) map1
+  let !map2 = shrinkMap d (mkStdGen 2) map1
   print $ show $ size map2
-  print $ show $ maximum $ elems map2
   time2 <- getCurrentTime
   print $ show $ diffUTCTime time2 time1
 
   print "regrowing"
   time1 <- getCurrentTime
-  let map3 = growMap n (randomKeysS (k1, k2) $ mkStdGen 3) map2
+  let !map3 = growMap n (randomKeysS (k1, k2) $ mkStdGen 3) map2
   print $ show $ size map3
-  print $ show $ maximum $ elems map3
   time2 <- getCurrentTime
   print $ show $ diffUTCTime time2 time1
-  
+
   print "total time"
   print $ show $ diffUTCTime time2 time0
